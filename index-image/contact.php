fd4e61e79f4c450a5f67ea32e68c607f93134997
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-06-18 17:23:48 -0700

    Add gifs/logos and modify form submission functionality

--- index-image/contact.php
+++ index-image/contact.php
@@ -4,20 +4,20 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'demo@domain.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'rohit.rekhi@gmail.com';
 
 // subject of the email
-$subject = 'New message from contact form';
+$subject = 'New message from portfolio site';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); 
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message');
 
 // message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$okMessage = 'Thanks for reaching out! I\'ll get back to you soon.';
 
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
@@ -33,11 +33,11 @@ try
 {
 
     if(count($_POST) == 0) throw new \Exception('Form is empty');
-            
+
     $emailText = "You have a new message from your contact form\n=============================\n";
 
     foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
+        // If the field exists in the $fields array, include it in the email
         if (isset($fields[$key])) {
             $emailText .= "$fields[$key]: $value\n";
         }
@@ -49,7 +49,7 @@ try
         'Reply-To: ' . $from,
         'Return-Path: ' . $from,
     );
-    
+
     // Send email
     mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
